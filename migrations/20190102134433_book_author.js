801dfcd5f3093737f1793e08fb887784968c460f
chris <verzetem@gmail.com>
2019-01-03 14:32:47 -0700

    added new book_author table and route

--- migrations/20190102134433_book_author.js
+++ migrations/20190102134433_book_author.js
@@ -1,6 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('book_author', function (table) {
+	  table.increments()
+	  table.integer('book_id').references('book.id').unsigned().onDelete('cascade')
+	  table.integer('author_id').references('author.id').unsigned().onDelete('cascade')
+	})
 };
 
 exports.down = function(knex, Promise) {
---
+++ seeds/02_book_author.js
@@ -0,0 +1,50 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        {
+          id: 1,
+          book_id: 1,
+          author_id: 1
+        },
+        {
+          id: 2,
+          book_id: 1,
+          author_id: 2
+        },
+        {
+          id: 3,
+          book_id: 1,
+          author_id: 3
+        },
+        {
+          id: 4,
+          book_id: 2,
+          author_id: 4
+        },
+        {
+          id: 5,
+          book_id: 3,
+          author_id: 5
+        },
+        {
+          id: 6,
+          book_id: 4,
+          author_id: 6
+        },
+        {
+          id: 7,
+          book_id: 5,
+          author_id: 6
+        },
+        {
+          id: 8,
+          book_id: 6,
+          author_id: 6
+        },
+      ]);
+    });
+};
